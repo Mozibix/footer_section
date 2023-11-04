13347a228f96c87f72e331ce54fee91f45544b2d
Mozibix <opuzmoses@gmail.com>
2023-11-04 13:05:39 +0100

    chores: updated index.tsx component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,9 @@
+import HomePage from "./HomePage";
+
 export default function Home() {
   return (
     <>
-      <h1 className="font-extrabold text-[3rem] uppercase">hello </h1>
-      <p> world</p>
+      <HomePage />
     </>
   );
 }
