94e11b3dbbc399f96b89d4d270bad8ccf96e2a89
Mozibix <opuzmoses@gmail.com>
2023-11-04 13:04:18 +0100

    chores: updated homepage component

---
+++ pages/HomePage.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Header } from "./(components)/Header";
+import { Hero } from "./(components)/Hero";
+import { Footer } from "./(components)/Footer";
+import { WhyChooseUs } from "./(components)/WhyChooseUs";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        {/* HEADER SECTION */}
+        <Header />
+
+        {/* MAIN SECTION */}
+
+        {/* HERO SECTION */}
+        <section className="hero_sec">
+          <Hero />
+        </section>
+
+        {/* WHY CHOOSE US SECTION */}
+        <section className="why_choose_us_sec">
+          <WhyChooseUs />
+        </section>
+
+        {/* FOOTER */}
+        <>
+          <Footer />
+        </>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
