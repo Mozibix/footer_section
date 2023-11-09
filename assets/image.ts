662734d16f63413717dd839ea096bd8624a5829a
Mozibix <opuzmoses@gmail.com>
2023-11-09 06:14:09 +0100

    chores: updated image imports component

---
+++ assets/image.ts
@@ -0,0 +1,9 @@
+export { default as apple_icon } from "./apple_icon.png";
+export { default as logo_icon } from "./logo_icon.png";
+export { default as phone_icon1 } from "./phone_icon1.png";
+export { default as phone_icon2 } from "./phone_icon2.png";
+export { default as playstore_icon } from "./playstore_icon.png";
+export { default as social_icon1 } from "./social_icon1.png";
+export { default as social_icon2 } from "./social_icon2.png";
+export { default as social_icon3 } from "./social_icon3.png";
+export { default as social_icon4 } from "./social_icon4.png";
