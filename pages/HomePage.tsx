6023aa2dc5659e1009141a6fa52b5e26167c99b8
Mozibix <opuzmoses@gmail.com>
2023-11-10 01:41:47 +0100

    chores: updated homepage component

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,27 +1,48 @@
-import React from "react";
+"use client";
+
+import React, { useEffect } from "react";
 import Header from "./(components)/Header";
 import Hero from "./(components)/Hero";
 import Footer from "./(components)/Footer";
 import WhyChooseUs from "./(components)/WhyChooseUs";
+import "aos/dist/aos.css";
 
 const HomePage = () => {
+  useEffect(() => {
+    import("aos").then((AOS) => {
+      AOS.init({
+        disable: false,
+        startEvent: "DOMContentLoaded",
+        initClassName: "aos-init",
+        animatedClassName: "aos-animate",
+        useClassNames: false,
+        disableMutationObserver: false,
+        debounceDelay: 50,
+        throttleDelay: 99,
+        offset: 120,
+        delay: 0,
+        duration: 400,
+        easing: "ease",
+        once: false,
+        mirror: false,
+        anchorPlacement: "top-bottom",
+      });
+    });
+  }, []);
+
   return (
     <>
       <div>
         {/* HEADER SECTION */}
-        <Header />
+        {/* <Header /> */}
 
         {/* MAIN SECTION */}
 
         {/* HERO SECTION */}
-        <section className="hero_sec">
-          <Hero />
-        </section>
+        <section className="hero_sec">{/* <Hero /> */}</section>
 
         {/* WHY CHOOSE US SECTION */}
-        <section className="why_choose_us_sec">
-          <WhyChooseUs />
-        </section>
+        <section className="why_choose_us_sec">{/* <WhyChooseUs /> */}</section>
 
         {/* FOOTER */}
         <>
